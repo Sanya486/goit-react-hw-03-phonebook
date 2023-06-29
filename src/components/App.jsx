53e8de1dd9ed6918ceb290b59061f09384bf0b00
Sanya486 <120394126+Sanya486@users.noreply.github.com>
2023-06-30 01:50:52 +0300

    add connection to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,20 +11,29 @@ export default class App extends Component {
 
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() { 
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts: contacts });
+    }    
+  }
 
   addNewContact = (id, name, number) => {
     const isContactExist = this.state.contacts.some(contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase())
     
     if (!isContactExist) {
       this.setState(prevState => {
-      return { contacts: [{ id, name, number }, ...prevState.contacts ] };
+        const newContactsObject = [{ id, name, number }, ...prevState.contacts] 
+        localStorage.setItem('contacts', JSON.stringify(newContactsObject));
+      return {contacts : newContactsObject};
     });
     }
     else {
@@ -34,18 +43,20 @@ export default class App extends Component {
   };
 
   onFilterChange = e => {
-    this.setState({ filter: e.target.value });
+    this.setState({ filter: e.target.value.toLowerCase() });
   };
 
   onActiveFilter = () => {
-    return this.state.contacts.filter(contact => contact.name.includes(this.state.filter))
+    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter))
   }
 
   deleteContact = (id) => {
     this.setState(prevState => {
+      const filteredContacts = prevState.contacts.filter(contact => contact.id !== id)
+      localStorage.setItem('contacts', JSON.stringify(filteredContacts));
       return {
-        contacts: prevState.contacts.filter(contact => contact.id !== id)
-      }
+        contacts: filteredContacts,
+      };
     })
   }
 
